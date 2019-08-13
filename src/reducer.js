14dac88d8b917dcf7f77dcfb29d3147df05cb202
langyo <langyo.china@gmail.com>
2019-08-13 18:39:50 +0800

    排除了一些错误，但搜索算法仍然不够完整

--- src/reducer.js
+++ src/reducer.js
@@ -87,66 +87,48 @@ export default (state = initStore, action) => {
       let translated = [];
       // 拆分各行
       let list = action.value.split('\n');
-      console.log('List:', list);
       // 遍历各行
       for (let line of list) {
         // 节点储存变量
         let nodes = [];
         // 对每个关键词进行枚举遍历
-        for(let word of state.pages.main.words) {
-          // 开火车，对每一个与当前关键字同长度的子字符串进行比较
-          for(let i = 0, j = word.length; j <= line.length; ++i, ++j) {
-            let splited = line.substr(i, j);
-            if(splited == word) {
-              // 匹配成功，检查占位冲突
-              let success = true;
-              for(let node of nodes) {
-                if(node.form <= i && i < node.to || node.form <= j && j < node.to) {
-                  success = false;
-                  break;
-                }
-              }
-              if(success) {
-                // 占位检测通过，将自己的占位信息写入 nodes
-                nodes.push({
-                  from: i,
-                  to: j,
-                  word
-                })
+        for (let word of state.pages.main.words) {
+          // 扫描生成一份坐标列表
+          let points = [];
+          for (let p = line.indexOf(word); p != -1; points.push(p), p = line.indexOf(word, p + 1));
+          // 转换坐标表为完整的坐标
+          points = points.map(p => ({ from: p, to: p + word.length, word }));
+          // 进行冲突检查
+          points = points.filter(p => {
+            let success = true;
+            for (let node of nodes) {
+              if (node.form <= p.from && p.from < node.to || node.form <= p.to && p.to < node.to) {
+                success = false;
+                break;
               }
             }
-          }
+            return success;
+          });
+          // 合并入节点列表
+          nodes = nodes.concat(points);
         }
 
         // 整理节点列表，进行排序
         nodes.sort((l, r) => {
-          if(l.from < r.from) return -1;
-          if(l.from > r.from) return 1;
+          if (l.from < r.from) return -1;
+          if (l.from > r.from) return 1;
 
           // 理论上，下头这些都不应当执行得到
           console.warn("执行到了不该执行到的地方！QAQ");
-          if(l.to < r.to) return -1;
-          if(l.to > r.to) return 1;
+          if (l.to < r.to) return -1;
+          if (l.to > r.to) return 1;
           throw new Error("不允许存在相等的填充坐标！");
         });
-        console.log('Nodes:', nodes);
 
-        // 填充节点并加进返回值中
-        if(nodes[0] && nodes[0].from != 0) nodes.unshift({ from: 0, to: nodes[0].to - 1 });
-        translated.push(nodes.reduce((prev, next) => {
-          // 空数组检查
-          if(prev.length < 1) return [next];
-
-          // 空隙检查
-          if(prev[prev.length - 1].to + 1 < next.from) prev.push({
-            from: prev[prev.length - 1].to + 1,
-            to: next.from - 1,
-            text: line.substr(prev[prev.length - 1].to + 1, next.from - 1)
-          }), console.log('!', prev[prev.length - 1]);
-
-          prev.push(next);
-          return prev;
-        }, []));
+        // 填充节点，将非关键词部分也作为节点填充进节点列表
+        // 待完成
+        
+        translated.push(nodes);
       }
       return {
         ...state,
